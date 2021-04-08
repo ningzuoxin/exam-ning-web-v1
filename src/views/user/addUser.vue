32e83f617094df3c7da959795234249aed47720b
zuoxinning <zuoxinning@upchina.com>
2021-04-08 16:43:00 +0800

    first commit

---
+++ src/views/user/addUser.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="app-container">
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="账号">
+        <el-input v-model="form.name"/>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="form.name"/>
+      </el-form-item>
+      <el-form-item label="邮箱">
+        <el-input v-model="form.name"/>
+      </el-form-item>
+      <el-form-item label="手机">
+        <el-input v-model="form.name"/>
+      </el-form-item>
+      <el-form-item label="昵称">
+        <el-input v-model="form.name"/>
+      </el-form-item>
+      <el-form-item label="性别">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="男"/>
+          <el-radio label="女"/>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">添加</el-button>
+        <el-button @click="onCancel">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AddUser',
+    data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        this.$message('submit!')
+      },
+      onCancel() {
+        this.$message({
+          message: 'cancel!',
+          type: 'warning'
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/user/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-input placeholder="Title" style="width: 200px;" class="filter-item"/>
+      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-search">
+        查询
+      </el-button>
+      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit">
+        添加
+      </el-button>
+      <el-button class="filter-item" type="primary" icon="el-icon-download">
+        导出
+      </el-button>
+    </div>
+
+    <el-table
+      :data="tableData"
+      height="85%"
+      border
+      style="width: 100%">
+      <el-table-column
+        type="selection"
+        align="center"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column label="操作" align="center" width="240" class-name="small-padding fixed-width">
+        <template>
+          <el-button type="primary" size="mini">
+            修改
+          </el-button>
+          <el-button size="mini" type="success">
+            发布
+          </el-button>
+          <el-button size="mini" type="danger">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div style="text-align: right;margin-top: 15px;">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="1000">
+      </el-pagination>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Index',
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
