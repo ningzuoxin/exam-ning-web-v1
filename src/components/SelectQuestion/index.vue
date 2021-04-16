be04788c02355eafc72cff85197a9eed88627b2c
zuoxinning <zuoxinning@upchina.com>
2021-04-16 11:03:06 +0800

    添加选择试题组件；

---
+++ src/components/SelectQuestion/index.vue
@@ -0,0 +1,469 @@
+<template>
+  <div>
+    <div class="box-start filter-container">
+      <el-select v-model="QusListQuery.type" placeholder="请选择试题类型">
+        <el-option v-for="item in options2" :key="item.value" :label="item.label" :value="item.value"/>
+      </el-select>
+      <el-input v-model="QusListQuery.searchText" class="Mr-16 Ml-20 rest" placeholder="关键字" prefix-icon="el-icon-search"/>
+      <el-button type="primary" class="searchButton" style="background-color: #3D53E6;" @click="handleFilter">搜索</el-button>
+    </div>
+    <el-table
+      ref="table"
+      :data="tableData"
+      stripe
+      height="398"
+      size="mini"
+      style="min-width: 100%"
+      @row-click="choseRow"
+      @select="selectCheck"
+      @select-all="selectCheckAll"
+      @selection-change="handleSelectionChange">
+      <el-table-column
+        v-if="isCheck"
+        type="selection"
+        align="center"
+        width="55"/>
+      <el-table-column
+        label="题型"
+        prop="type"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        label="题干"
+        prop="stem">
+      </el-table-column>
+      <el-table-column
+        label="创建时间"
+        prop="createTime"
+        width="130">
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      v-if="QusListQuery.total >= 0"
+      :current-page="QusListQuery.currentPage"
+      :page-size="QusListQuery.pageSize"
+      :total="QusListQuery.total"
+      style="margin-top: 20px;text-align: center;"
+      layout="total, prev, pager, next, sizes, jumper"
+      @current-change="handleCurrentChange"
+      @size-change="handleSizesChange"/>
+
+    <div class="W100 scoreContainer">
+      <div class="box-start Mb-20">
+        <div>选择：</div>
+        <div class="mousePointer">全选</div>
+        <div>-</div>
+        <div class="mousePointer">取消</div>
+      </div>
+      <div class="box-start Mb-20">
+        <div class="Mr-20">当前已选中：{{ multipleSelectionAll.length }}道题目</div>
+        <div>共计{{ allScore }}分</div>
+      </div>
+      <div class="box-start">
+        <div class="itemCss">
+          <div class="Mb-20">题型：</div>
+          <div class="Mb-20">题数：</div>
+          <div class="Mb-20 box-start" style="height:25px">分数：</div>
+        </div>
+        <div class="itemCss">
+          <div class="Mb-20">单选题</div>
+          <div class="Mb-20">{{ scoreObj.singleNum }}</div>
+          <div class="Mb-20">
+            <input v-model.number="scoreObj.singleScore" type="text" class="scoreInput">
+          </div>
+
+        </div>
+        <div class="itemCss">
+          <div class="Mb-20">多选题</div>
+          <div class="Mb-20">{{ scoreObj.moreNum }}</div>
+          <div class="Mb-20"><input v-model.number="scoreObj.moreScore" type="text" class="scoreInput"></div>
+        </div>
+        <div class="itemCss">
+          <div class="Mb-20">判断题</div>
+          <div class="Mb-20">{{ scoreObj.judgNum }}</div>
+          <div class="Mb-20"><input v-model.number="scoreObj.judgScore" type="text" class="scoreInput"></div>
+        </div>
+        <div class="itemCss">
+          <div class="Mb-20">填空题</div>
+          <div class="Mb-20">{{ scoreObj.blankNum }}</div>
+          <div class="Mb-20"><input v-model.number="scoreObj.blankScore" type="text" class="scoreInput"></div>
+        </div>
+        <div class="itemCss">
+          <div class="Mb-20">问答题</div>
+          <div class="Mb-20">{{ scoreObj.answerNum }}</div>
+          <div class="Mb-20"><input v-model.number="scoreObj.answerScore" type="text" class="scoreInput"></div>
+        </div>
+      </div>
+    </div>
+    <div class="box-end">
+      <el-button v-if="isCheck" type="primary" @click="sureSelect">确定</el-button>
+    </div>
+  </div>
+</template>
+<script>
+  import Url from '@/api/url'
+  import { listQuestion } from '@/api/testPaper/question'
+
+  export default {
+    name: 'SelectQuestion',
+    props: {
+      selecttype: {
+        type: String,
+        default: null
+      },
+      dateStamp: {
+        type: Number,
+        default: null
+      },
+      selectData: {
+        type: Array,
+        default: null
+      }
+    },
+    data() {
+      return {
+        tableData: [],
+        QusListQuery: {
+          currentPage: 1,
+          creator: null,
+          search: null,
+          pageSize: Url.pageSize,
+          categoryId: null,
+          total: null,
+          searchText: null,
+          type: null
+        },
+        selectType: 'radio',
+        isShowChapter: false,
+        isShowTopicLook: false,
+        questionType: 1,
+        topicDetail: null,
+        multipleSelectionAll: [], // 所有选中的数据包含跨页数据
+        selection: [], // 当前页选中的数据
+        idKey: 'id', // 标识列表数据中每一行的唯一键的名称(需要按自己的数据改一下)
+        options: [{
+          value: '内部讲师',
+          label: '内部讲师'
+        }, {
+          value: '外部讲师',
+          label: '外部讲师'
+        }, {
+          value: '认证讲师',
+          label: '认证讲师'
+        }],
+        options2: [{
+          value: 'choice',
+          label: '单选题'
+        }, {
+          value: 'choice_multi',
+          label: '多选题'
+        }, {
+          value: 'true_false',
+          label: '判断题'
+        }, {
+          value: 'fill_blank',
+          label: '填空题'
+        }, {
+          value: 'question',
+          label: '问答题'
+        }],
+        value: null,
+        searchValue: null,
+        searchValue2: null,
+        scoreObj: {
+          singleScore: 0,
+          moreScore: 0,
+          missMoreScore: 0,
+          judgScore: 0,
+          blankScore: 0,
+          answerScore: 0,
+          singleNum: 0,
+          moreNum: 0,
+          judgNum: 0,
+          blankNum: 0,
+          answerNum: 0
+        }
+      }
+    },
+    computed: {
+      isCheck() {
+        if (this.selecttype === 'radio') {
+          return false
+        } else {
+          return true
+        }
+      },
+      treeDataList() {
+        return this.$store.getters.chapterType
+      },
+      allScore() {
+        const temp = this.scoreObj.singleScore * this.scoreObj.singleNum +
+          this.scoreObj.moreScore * this.scoreObj.moreNum +
+          this.scoreObj.judgScore * this.scoreObj.judgNum +
+          this.scoreObj.blankScore * this.scoreObj.blankNum +
+          this.scoreObj.answerScore * this.scoreObj.answerNum
+        return temp
+      }
+    },
+    watch: {
+      dateStamp(val) {
+        this.multipleSelectionAll = []
+        this.selection = []
+        this.scoreObj = {
+          singleScore: 0,
+          moreScore: 0,
+          judgScore: 0,
+          blankScore: 0,
+          answerScore: 0,
+          singleNum: 0,
+          moreNum: 0,
+          judgNum: 0,
+          blankNum: 0,
+          answerNum: 0
+        }
+        this.getList()
+      },
+      multipleSelectionAll: {
+        handler(val) {
+          this.scoreObj.singleNum = 0
+          this.scoreObj.moreNum = 0
+          this.scoreObj.judgNum = 0
+          this.scoreObj.blankNum = 0
+          this.scoreObj.answerNum = 0
+          for (let i = 0; i < val.length; i++) {
+            if (val[i].type === 'choice') {
+              this.scoreObj.singleNum = this.scoreObj.singleNum + 1
+            } else if (val[i].type === 'choice_multi') {
+              this.scoreObj.moreNum = this.scoreObj.moreNum + 1
+            } else if (val[i].type === 'true_false') {
+              this.scoreObj.judgNum = this.scoreObj.judgNum + 1
+            } else if (val[i].type === 'fill_blank') {
+              this.scoreObj.blankNum = this.scoreObj.blankNum + 1
+            } else if (val[i].type === 'question') {
+              this.scoreObj.answerNum = this.scoreObj.answerNum + 1
+            }
+          }
+        },
+        deep: true
+      }
+    },
+    created() {
+      this.getList()
+    },
+    methods: {
+      resetFilter() {
+        const that = this
+        that.QusListQuery = {
+          currentPage: 1,
+          creator: null,
+          search: null,
+          pageSize: Url.pageSize,
+          categoryId: null,
+          total: null,
+          searchText: null,
+          type: null
+        }
+      },
+      getList() {
+        this.tableData = listQuestion()
+        // this.$post(Url.examquestions.query, this.QusListQuery).then(data => {
+        //   console.log(data)
+        //   const result = data.data
+        //   this.tableData = result.records
+        //   this.QusListQuery.total = result.total
+        //   this.QusListQuery.currentPage = result.current
+        //   this.QusListQuery.pageSize = result.size
+        //   setTimeout(() => {
+        //     this.setSelectRow()
+        //   }, 200)
+        // })
+      },
+      handleFilter() {
+        this.QusListQuery.currentPage = 1
+        this.getList()
+      },
+      choseRow(row, event, column) {
+        if (this.isCheck === true) {
+          return
+        }
+        console.log(row)
+        console.log(event)
+        console.log(column)
+        this.$emit('giveTopic', row)
+      },
+      sureSelect() {
+        if (this.scoreObj.singleNum !== 0) {
+          if (this.scoreObj.singleScore === 0 || !this.scoreObj.singleScore) {
+            this.$message('请将分数填写完整')
+            return
+          }
+        }
+        if (this.scoreObj.moreNum !== 0) {
+          if (this.scoreObj.moreScore === 0 || !this.scoreObj.moreScore) {
+            this.$message('请将分数填写完整')
+            return
+          }
+        }
+        if (this.scoreObj.judgNum !== 0) {
+          if (this.scoreObj.judgScore === 0 || !this.scoreObj.judgScore) {
+            this.$message('请将分数填写完整')
+            return
+          }
+        }
+        if (this.scoreObj.blankNum !== 0) {
+          if (this.scoreObj.blankScore === 0 || !this.scoreObj.blankScore) {
+            this.$message('请将分数填写完整')
+            return
+          }
+        }
+        if (this.scoreObj.answerNum !== 0) {
+          if (this.scoreObj.answerScore === 0 || !this.scoreObj.answerScore) {
+            this.$message('请将分数填写完整')
+            return
+          }
+        }
+        this.getAllSelectionData()
+        const tempObj = {
+          multipleSelectionAll: this.$copy(this.multipleSelectionAll),
+          scoreObj: this.scoreObj
+        }
+        this.$emit('giveTopic', tempObj)
+      },
+      selectCheck(selection, row) {
+        this.selection = selection
+        this.getAllSelectionData()
+        console.log(selection, row)
+      },
+      showChapter() {
+        this.isShowChapter = true
+      },
+      giveChapter(data) {
+        this.isShowChapter = false
+        this.QusListQuery.chapterId = data.id
+        this.QusListQuery.chapterName = data.name
+        console.log(data)
+      },
+      selectCheckAll(selection) {
+        this.selection = selection
+        this.getAllSelectionData()
+      },
+      // 记忆多选方法
+      // 设置选中的方法
+      setSelectRow() {
+        if (this.selecttype === 'radio') {
+          return
+        }
+        if (!this.multipleSelectionAll || this.multipleSelectionAll.length <= 0) {
+          return
+        }
+        // 标识当前行的唯一键的名称
+        const idKey = this.idKey
+        const selectAllIds = []
+        this.multipleSelectionAll.forEach(row => {
+          selectAllIds.push(row[idKey])
+        })
+        this.$refs.table.clearSelection()
+        for (var i = 0; i < this.tableData.length; i++) {
+          if (selectAllIds.indexOf(this.tableData[i][idKey]) >= 0) {
+            // 设置选中，记住table组件需要使用ref="table"
+            this.$refs.table.toggleRowSelection(this.tableData[i], true)
+          }
+        }
+      },
+      // 记忆选择核心方法
+      changePageCoreRecordData() {
+        // 标识当前行的唯一键的名称
+        if (this.selecttype === 'radio') {
+          return
+        }
+        const idKey = this.idKey
+        const that = this
+        // 如果总记忆中还没有选择的数据，那么就直接取当前页选中的数据，不需要后面一系列计算
+        if (this.multipleSelectionAll.length <= 0) {
+          this.multipleSelectionAll = this.selection
+          return
+        }
+        // 总选择里面的key集合
+        const selectAllIds = []
+        this.multipleSelectionAll.forEach(row => {
+          selectAllIds.push(row[idKey])
+        })
+        const selectIds = []
+        // 获取当前页选中的id
+        this.selection.forEach(row => {
+          selectIds.push(row[idKey])
+          // 如果总选择里面不包含当前页选中的数据，那么就加入到总选择集合里
+          if (selectAllIds.indexOf(row[idKey]) < 0) {
+            that.multipleSelectionAll.push(row)
+          }
+        })
+        const noSelectIds = []
+        // 得到当前页没有选中的id
+        this.tableData.forEach(row => {
+          if (selectIds.indexOf(row[idKey]) < 0) {
+            noSelectIds.push(row[idKey])
+          }
+        })
+        noSelectIds.forEach(id => {
+          if (selectAllIds.indexOf(id) >= 0) {
+            for (let i = 0; i < that.multipleSelectionAll.length; i++) {
+              if (that.multipleSelectionAll[i][idKey] === id) {
+                // 如果总选择中有未被选中的，那么就删除这条
+                that.multipleSelectionAll.splice(i, 1)
+                break
+              }
+            }
+          }
+        })
+      },
+      handleCurrentChange(value) {
+        // 改变页的时候调用一次
+        this.changePageCoreRecordData()
+        this.QusListQuery.currentPage = value
+        this.getList()
+      },
+      handleSizesChange(value) {
+        // 改变页的时候调用一次
+        this.changePageCoreRecordData()
+        this.QusListQuery.pageSize = value
+        this.getList()
+      },
+      handleSelectionChange(selection) {
+        this.selection = selection
+        this.getAllSelectionData()
+      },
+      // 得到选中的所有数据
+      getAllSelectionData() {
+        // 再执行一次记忆勾选数据匹配，目的是为了在当前页操作勾选后直接获取选中数据
+        this.changePageCoreRecordData()
+        console.log(this.multipleSelectionAll)
+      }
+    }
+  }
+</script>
+<style scoped>
+  .itemCss {
+    width: 65px;
+  }
+
+  .scoreInput {
+    width: 40px;
+    height: 25px;
+    border-radius: 5px;
+    border: 1px solid #dadada;
+    padding: 5px 10px
+  }
+
+  .scoreInput:focus {
+    outline: none
+  }
+
+  .scoreContainer {
+    background-color: antiquewhite;
+    padding: 10px;
+    margin-bottom: 20px;
+    margin-top: 10px
+  }
+</style>
+
--- src/views/testPaper/addTestPaper.vue
+++ src/views/testPaper/addTestPaper.vue
@@ -1,40 +1,49 @@
 <template>
   <div class="app-container">
-    <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="账号">
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="试卷类型">
+        <el-select placeholder="请选择试卷类型">
+          <el-option label="正式考试" value="shanghai"/>
+          <el-option label="模拟考试" value="beijing"/>
+          <el-option label="刷题学习" value="beijing"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="试卷名称">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
+      <el-form-item label="限时">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
-      <el-form-item label="邮箱">
+      <el-form-item label="及格分">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
-      <el-form-item label="手机">
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item label="昵称">
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item label="性别">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="男"></el-radio>
-          <el-radio label="女"></el-radio>
-        </el-radio-group>
+      <el-form-item label="选择试题">
+        <el-button type="primary" @click="showDialog">选择试题</el-button>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">添加</el-button>
         <el-button @click="onCancel">重置</el-button>
       </el-form-item>
     </el-form>
+    <!-- 选择试题弹出层 -->
+    <el-dialog :visible.sync="isShowDialog" :append-to-body="true" :close-on-click-modal="false" title="试题列表" width="1000px" style="padding: 0px;">
+      <SelectQuestion :selecttype="selectType" :date-stamp="dataTime" :select-data="examQuestionList"/>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+  import SelectQuestion from '@/components/SelectQuestion'
+
   export default {
     name: 'AddTestPaper',
+    components: { SelectQuestion },
     data() {
       return {
+        isShowDialog: false,
+        selectType: 'check',
+        dataTime: null,
+        examQuestionList: [],
         form: {
           name: '',
           region: '',
@@ -56,6 +65,10 @@
           message: 'cancel!',
           type: 'warning'
         })
+      },
+      showDialog() {
+        this.dataTime = new Date().getTime()
+        this.isShowDialog = true
       }
     }
   }
