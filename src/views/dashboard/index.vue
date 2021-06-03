0cc4569455992b1f435e47976552972307cc7dd9
zuoxinning <zuoxinning@upchina.com>
2021-06-03 11:15:18 +0800

    移除一些没用的组件；

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,30 +1,31 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">欢迎使用微服务考试平台</div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+  import { mapGetters } from 'vuex'
 
-export default {
-  name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
+  export default {
+    name: 'Dashboard',
+    computed: {
+      ...mapGetters([
+        'name'
+      ])
+    }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
+  .dashboard {
+    &-container {
+      margin: 30px;
+    }
+
+    &-text {
+      font-size: 30px;
+      line-height: 46px;
+    }
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
 </style>
